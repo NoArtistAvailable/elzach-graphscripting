a88119b6f08506a403365014de5067fcf3479853
reinemann <reinemann@prefrontalcortex.de>
2021-08-30 12:41:36 +0200

    [TransformNode] made translation a movement by evaluation instead of coroutine

--- package/Transform/TranslateTransform.cs
+++ package/Transform/TranslateTransform.cs
@@ -15,41 +15,51 @@ namespace elZach.GraphScripting
         public Vector3 toMove;
         public AnimationCurve movementCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(1f, 1f));
         public float time = 0.5f;
-        private Coroutine movementRoutine;
+        
+        //private Coroutine movementRoutine;
+
+        private float progress;
+        private Vector3 startPosition;
+        private Vector3 targetPosition;
 
         public override Color GetColor() => new Color(0.3f,0.5f,0.4f);
-
-        // public override List<TreeContainer.Parameter> GetPublicParameters()
-        // {
-        //     return new List<TreeContainer.Parameter>() { new TreeContainer.Parameter(){name = targetTransformBindingName, type = typeof(Transform)} };
-        // }
-
         protected override void OnStart()
         {
-            if(target)
-                movementRoutine = director.StartCoroutine(DoTheMove());
+            if (!target) return;
+            progress = 0f;
+            startPosition = target.position;
+            targetPosition = startPosition + toMove;
+
         }
 
         protected override State OnUpdate()
         {
             if (!target) return State.Failure;
-            if (movementRoutine != null) return State.Running;
+            //if (movementRoutine != null) return State.Running;
+            if (progress < 1f)
+            {
+                progress += Time.deltaTime / time; //<- should be evaluation delta Time
+                target.position = Vector3.Lerp(startPosition, targetPosition, progress);
+                return State.Running;
+            }
+
+            target.position = targetPosition;
             return State.Success;
         }
 
-        IEnumerator DoTheMove()
-        {
-            float progress = 0f;
-            Vector3 startPosition = target.position;
-            Vector3 targetPosition = startPosition + toMove;
-            while (progress < 1f)
-            {
-                progress += Time.deltaTime / time;
-                target.position = Vector3.Lerp(startPosition, targetPosition, progress);
-                yield return null;
-            }
-            target.position = targetPosition;
-            movementRoutine = null;
-        }
+        // IEnumerator DoTheMove()
+        // {
+        //     float progress = 0f;
+        //     Vector3 startPosition = target.position;
+        //     Vector3 targetPosition = startPosition + toMove;
+        //     while (progress < 1f)
+        //     {
+        //         progress += Time.deltaTime / time;
+        //         target.position = Vector3.Lerp(startPosition, targetPosition, progress);
+        //         yield return null;
+        //     }
+        //     target.position = targetPosition;
+        //     movementRoutine = null;
+        // }
     }
 }
